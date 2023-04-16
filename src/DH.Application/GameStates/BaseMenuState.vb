7345267317d6d2c435caafac9bcb6f16357f5888
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-04-16 15:43:13 -0500

    main menu becamse base menu derived

---
+++ src/DH.Application/GameStates/BaseMenuState.vb
@@ -0,0 +1,37 @@
+﻿Friend MustInherit Class BaseMenuState
+    Inherits BaseGameState(Of Hue, Command, Sfx, GameState)
+    Private ReadOnly _menuItems As IReadOnlyList(Of String)
+    Private _currentItem As Integer = 0
+
+    Protected Sub New(parent As IGameController(Of Hue, Command, Sfx), setState As Action(Of GameState), menuItems As IReadOnlyList(Of String))
+        MyBase.New(parent, setState)
+        _menuItems = menuItems
+    End Sub
+
+    MustOverride Sub HandleMenuItem(menuItem As String)
+
+    Public Overrides Sub HandleCommand(command As Command)
+        Select Case command
+            Case Command.UpReleased
+                _currentItem = (_currentItem + _menuItems.Count - 1) Mod _menuItems.Count
+            Case Command.DownReleased
+                _currentItem = (_currentItem + 1) Mod _menuItems.Count
+            Case Command.FireReleased
+                HandleMenuItem(_menuItems(_currentItem))
+        End Select
+    End Sub
+
+    Public Overrides Sub Render(displayBuffer As IPixelSink(Of Hue))
+        displayBuffer.Fill((0, 0), (ViewWidth, ViewHeight), Hue.Black)
+        Dim font = Fonts(GameFont.Font5x7)
+        Dim row = 0
+        For Each menuItem In _menuItems
+            Dim h As Hue = If(row = _currentItem, Hue.LightBlue, Hue.Blue)
+            font.WriteText(displayBuffer, (0, row * font.Height), menuItem, h)
+            row += 1
+        Next
+    End Sub
+
+    Public Overrides Sub Update(elapsedTime As TimeSpan)
+    End Sub
+End Class
--- src/DH.Application/GameStates/MainMenuState.vb
+++ src/DH.Application/GameStates/MainMenuState.vb
@@ -1,50 +1,18 @@
 ﻿Friend Class MainMenuState
-    Inherits BaseGameState(Of Hue, Command, Sfx, GameState)
+    Inherits BaseMenuState
     Const EditText = "Edit"
     Const OptionsText = "Options"
     Const QuitText = "Quit"
     Const AboutText = "About"
-    Private ReadOnly _menuItems As IReadOnlyList(Of String) =
-        New List(Of String) From
-        {
-            EditText,
-            OptionsText,
-            AboutText,
-            QuitText
-        }
-    Private _currentMenuItem As Integer = 0
-
     Public Sub New(parent As IGameController(Of Hue, Command, Sfx), setState As Action(Of GameState))
-        MyBase.New(parent, setState)
+        MyBase.New(parent, setState, New List(Of String) From {EditText, OptionsText, AboutText, QuitText})
     End Sub
-
-    Public Overrides Sub HandleCommand(command As Command)
-        Select Case command
-            Case Command.UpReleased
-                _currentMenuItem = (_currentMenuItem + _menuItems.Count - 1) Mod _menuItems.Count
-            Case Command.DownReleased
-                _currentMenuItem = (_currentMenuItem + 1) Mod _menuItems.Count
-            Case Command.FireReleased
-                Select Case _menuItems(_currentMenuItem)
-                    Case EditText
-                        SetState(GameState.EditMenu)
-                    Case QuitText
-                        SetState(GameState.ConfirmQuit)
-                End Select
+    Public Overrides Sub HandleMenuItem(menuItem As String)
+        Select Case menuItem
+            Case EditText
+                SetState(GameState.EditMenu)
+            Case QuitText
+                SetState(GameState.ConfirmQuit)
         End Select
-    End Sub
-
-    Public Overrides Sub Render(displayBuffer As IPixelSink(Of Hue))
-        displayBuffer.Fill((0, 0), (ViewWidth, ViewHeight), Hue.Black)
-        Dim font = Fonts(GameFont.Font5x7)
-        Dim row = 0
-        For Each menuItem In _menuItems
-            Dim h = If(row = _currentMenuItem, Hue.LightBlue, Hue.Blue)
-            font.WriteText(displayBuffer, (0, font.Height * row), menuItem, h)
-            row += 1
-        Next
-    End Sub
-
-    Public Overrides Sub Update(elapsedTime As TimeSpan)
     End Sub
 End Class
