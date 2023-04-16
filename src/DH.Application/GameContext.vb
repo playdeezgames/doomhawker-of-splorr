b2c33b036da1312539dd4ddb5614499b6dc0e368
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-04-16 05:30:21 -0500

    Added some fonts.

--- src/DH.Application/GameContext.vb
+++ src/DH.Application/GameContext.vb
@@ -1,14 +1,15 @@
 Public Module GameContext
     Public Const ViewWidth = 160
     Public Const ViewHeight = 90
-    Public Const CellWidth = 5
-    Public Const CellHeight = 5
-    Public Const CellColumns = ViewWidth \ CellWidth
-    Public Const CellRows = ViewHeight \ CellHeight
-    Public X As Integer
-    Public Y As Integer
-    Public Sub Initialize()
-        X = CellColumns \ 2
-        Y = CellRows \ 2
+    Friend Sub Initialize()
+        InitializeFonts()
+    End Sub
+    Friend ReadOnly Fonts As New Dictionary(Of GameFont, Font)
+    Private Sub InitializeFonts()
+        Fonts.Clear()
+        Fonts.Add(GameFont.Font3x5, New Font(JsonSerializer.Deserialize(Of FontData)(File.ReadAllText("Content/CyFont3x5.json"))))
+        Fonts.Add(GameFont.Font4x6, New Font(JsonSerializer.Deserialize(Of FontData)(File.ReadAllText("Content/CyFont4x6.json"))))
+        Fonts.Add(GameFont.Font5x7, New Font(JsonSerializer.Deserialize(Of FontData)(File.ReadAllText("Content/CyFont5x7.json"))))
+        Fonts.Add(GameFont.Font8x8, New Font(JsonSerializer.Deserialize(Of FontData)(File.ReadAllText("Content/CyFont8x8.json"))))
     End Sub
 End Module
--- src/DH.Application/TitleState.vb
+++ src/DH.Application/TitleState.vb
@@ -6,21 +6,12 @@
     End Sub
 
     Public Overrides Sub HandleCommand(command As Command)
-        Select Case command
-            Case Command.Down
-                Y += 1
-            Case Command.Up
-                Y -= 1
-            Case Command.Right
-                X += 1
-            Case Command.Left
-                X -= 1
-        End Select
     End Sub
 
     Public Overrides Sub Render(displayBuffer As IPixelSink(Of Hue))
         displayBuffer.Fill((0, 0), (ViewWidth, ViewHeight), Hue.Black)
-        displayBuffer.Fill((X * CellWidth, Y * CellHeight), (CellWidth, CellHeight), Hue.Blue)
+        Dim font = Fonts(GameFont.Font8x8)
+        font.WriteText(displayBuffer, (0, 0), "Hello, world!", Hue.White)
     End Sub
 
     Public Overrides Sub Update(elapsedTime As TimeSpan)
