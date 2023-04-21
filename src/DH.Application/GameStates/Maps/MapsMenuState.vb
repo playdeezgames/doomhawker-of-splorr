7bac7ba2389ef28d6abedccb81bfebf8b23140b6
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-04-21 03:53:31 -0500

    map things are did

--- src/DH.Application/GameStates/Maps/MapsMenuState.vb
+++ src/DH.Application/GameStates/Maps/MapsMenuState.vb
@@ -1,7 +1,7 @@
 ﻿Friend Class MapsMenuState
     Inherits BaseMenuState
     Const NewMapText = "New Map..."
-    Const EditMapText = "Edit Map..."
+    Const EditMapText = "Pick Map..."
     Public Sub New(parent As IGameController(Of Hue, Command, Sfx), setState As Action(Of GameState))
         MyBase.New(parent, setState, New List(Of String) From {
                     NewMapText,
@@ -21,8 +21,12 @@
                 End If
         End Select
     End Sub
-
     Protected Overrides Sub HandleCancel()
         SetState(GameState.MainMenu)
+    End Sub
+    Public Overrides Sub Render(displayBuffer As IPixelSink(Of Hue))
+        MyBase.Render(displayBuffer)
+        Dim font = Fonts(GameFont.Font5x7)
+        font.WriteText(displayBuffer, (0, ViewHeight - font.Height), $"Map Count: {Editor.MapNames.Count}", Hue.White)
     End Sub
 End Class
