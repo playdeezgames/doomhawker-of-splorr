44c19a2f470d74dc023c4a3995b688d7ab8a6601
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-04-25 04:13:02 -0500

    avatar data exists

--- src/DH.Data/EditorData.vb
+++ src/DH.Data/EditorData.vb
@@ -4,6 +4,7 @@ Public Class EditorData
     Public Property Maps As New Dictionary(Of String, MapData)
     Public Property Items As New Dictionary(Of String, ItemData)
     Public Property Creatures As New Dictionary(Of String, CreatureData)
+    Public Property Avatar As AvatarData
     Public Property MapCellWidth As Integer = 8
     Public Property MapCellHeight As Integer = 8
 End Class
