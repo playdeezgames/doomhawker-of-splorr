c15caeaf65fdebe3f204fa69f0d83b659cca6269
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-05-11 14:23:41 -0500

    eliminate the unneeded

--- src/DH.Business/ColorEditor.vb
+++ src/DH.Business/ColorEditor.vb
@@ -2,12 +2,10 @@
     Inherits ThingieEditor(Of ColorData, IEditorColor)
     Implements IColorEditor
     Private ReadOnly _data As EditorData
-
     Public Sub New(data As EditorData)
         MyBase.New(Function() data.Colors, Function(name) New EditorColor(data, name))
-        Me._data = data
+        _data = data
     End Sub
-
     Public Function Create(name As String, red As Byte, green As Byte, blue As Byte) As IEditorColor Implements IColorEditor.Create
         _data.Colors(name) = New ColorData With
             {
