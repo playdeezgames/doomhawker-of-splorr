e69b1d77a140dddff9b7bdc41d7b8f7818a5b5da
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-05-11 03:29:03 -0500

    hues module

---
+++ src/DH.Application/Hues.vb
@@ -0,0 +1,19 @@
+﻿Friend Module Hues
+    Const Black = "black"
+    Const Blue = "blue"
+    Const Green = "green"
+    Const Cyan = "cyan"
+    Const Red = "red"
+    Const Magenta = "magenta"
+    Const Brown = "brown"
+    Const Gray = "gray"
+    Const DarkGray = "darkgray"
+    Const LightBlue = "lightblue"
+    Const LightGreen = "lightgreen"
+    Const LightCyan = "lightcyan"
+    Const LightRed = "lightred"
+    Const LightMagenta = "lightmagenta"
+    Const Yellow = "yellow"
+    Const White = "white"
+
+End Module
