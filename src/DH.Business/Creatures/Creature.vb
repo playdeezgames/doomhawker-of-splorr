2c49c218c923ec3198c0707019f85aff34e7b283
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-05-23 04:13:41 -0500

    creature refactored

--- src/DH.Business/Creatures/Creature.vb
+++ src/DH.Business/Creatures/Creature.vb
@@ -1,28 +1,23 @@
 ﻿Friend Class Creature
     Inherits Sprite
     Implements ICreature
-
-    Private ReadOnly _data As WorldData
-    Private ReadOnly _name As String
     Private ReadOnly Property CreatureData As CreatureData
         Get
-            Return _data.Creatures(_name)
+            Return WorldData.Creatures(_spriteName)
         End Get
     End Property
 
     Public Sub New(data As WorldData, name As String)
         MyBase.New(data, Function(w, n) w.Creatures(n), name)
-        Me._data = data
-        Me._name = name
     End Sub
 
     Public Function CreateInstance(mapName As String, column As Integer, row As Integer) As ICreatureInstance Implements ICreature.CreateInstance
-        Dim mapData = _data.Maps(mapName)
+        Dim mapData = WorldData.Maps(mapName)
         Dim mapCellData = mapData.Cells(column + row * mapData.Columns)
         mapCellData.Creature = New CreatureInstanceData With
             {
                 .CreatureName = Name
             }
-        Return New CreatureInstance(_data, mapName, column, row)
+        Return New CreatureInstance(WorldData, mapName, column, row)
     End Function
 End Class
