b79b005a1da90d49a94860cc630d83eccebb1e01
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-05-23 03:21:03 -0500

    world data client

--- src/DH.Business/World/World.vb
+++ src/DH.Business/World/World.vb
@@ -1,83 +1,83 @@
 ﻿Public Class World
+    Inherits WorldDataClient
     Implements IWorld
-    Private _data As WorldData
     Sub New(data As WorldData)
-        _data = data
+        MyBase.New(data)
     End Sub
     Public Property MapCellWidth As Integer Implements IWorld.MapCellWidth
         Get
-            Return _data.MapCellWidth
+            Return WorldData.MapCellWidth
         End Get
         Set(value As Integer)
-            _data.MapCellWidth = value
+            WorldData.MapCellWidth = value
         End Set
     End Property
     Public Property MapCellHeight As Integer Implements IWorld.MapCellHeight
         Get
-            Return _data.MapCellHeight
+            Return WorldData.MapCellHeight
         End Get
         Set(value As Integer)
-            _data.MapCellHeight = value
+            WorldData.MapCellHeight = value
         End Set
     End Property
     Public ReadOnly Property Fonts As IFontEditor Implements IWorld.Fonts
         Get
-            Return New FontEditor(_data)
+            Return New FontEditor(WorldData)
         End Get
     End Property
 
     Public ReadOnly Property Terrains As ITerrainEditor Implements IWorld.Terrains
         Get
-            Return New TerrainEditor(_data)
+            Return New TerrainEditor(WorldData)
         End Get
     End Property
 
     Public ReadOnly Property Maps As IMapEditor Implements IWorld.Maps
         Get
-            Return New MapEditor(_data)
+            Return New MapEditor(WorldData)
         End Get
     End Property
 
     Public ReadOnly Property Items As IItemEditor Implements IWorld.Items
         Get
-            Return New ItemEditor(_data)
+            Return New ItemEditor(WorldData)
         End Get
     End Property
 
     Public ReadOnly Property Creatures As ICreatureEditor Implements IWorld.Creatures
         Get
-            Return New CreatureEditor(_data)
+            Return New CreatureEditor(WorldData)
         End Get
     End Property
 
     Public ReadOnly Property Avatar As IAvatar Implements IWorld.Avatar
         Get
-            If _data.Avatar Is Nothing Then
+            If WorldData.Avatar Is Nothing Then
                 Return Nothing
             End If
-            Return New Avatar(_data)
+            Return New Avatar(WorldData)
         End Get
     End Property
 
     Public ReadOnly Property Colors As IColorEditor Implements IWorld.Colors
         Get
-            Return New ColorEditor(_data)
+            Return New ColorEditor(WorldData)
         End Get
     End Property
 
     Public Sub Save(fileName As String) Implements IWorld.Save
-        File.WriteAllText(fileName, JsonSerializer.Serialize(_data))
+        File.WriteAllText(fileName, JsonSerializer.Serialize(WorldData))
     End Sub
     Public Sub Load(fileName As String) Implements IWorld.Load
-        _data = JsonSerializer.Deserialize(Of WorldData)(File.ReadAllText(fileName))
+        WorldData = JsonSerializer.Deserialize(Of WorldData)(File.ReadAllText(fileName))
     End Sub
 
     Public Sub ClearAvatar() Implements IWorld.ClearAvatar
-        _data.Avatar = Nothing
+        WorldData.Avatar = Nothing
     End Sub
 
     Public Sub SetAvatar(creature As ICreatureInstance) Implements IWorld.SetAvatar
-        _data.Avatar = New AvatarData With
+        WorldData.Avatar = New AvatarData With
             {
                 .MapName = creature.MapName,
                 .Column = creature.Column,
